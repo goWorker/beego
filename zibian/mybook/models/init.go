f0a5527be81c2f4c2346b5f1477f5202d51254fb
allanyang <yangyanzhao@outlook.com>
2020-03-26 09:21:51 +0800

    chapter 3

--- zibian/mybook/models/init.go
+++ zibian/mybook/models/init.go
@@ -5,27 +5,52 @@ import (
 	"github.com/astaxie/beego/orm"
 )
 
-func init()  {
+func init() {
 	orm.RegisterModel(
 		new(Category),
 		new(Book),
+		new(Document),
+		new(Attachment),
+		new(DocumentStore),
 		new(BookCategory),
 		new(Member),
 		new(Collection),
+		new(Relationship),
 		new(Fans),
-		)
+		new(Comments),
+		new(Score),
+	)
 }
 
-func TNCategory()string{
+/*
+* Table Names
+* */
+
+func TNCategory() string {
 	return "md_category"
 }
 
-func TNBook()string{
+func TNBookCategory() string {
+	return "md_book_category"
+}
+
+func TNBook() string {
 	return "md_books"
 }
 
-func TNBookCategory() string{
-	return "md_book_category"
+func TNDocuments() string {
+	return "md_documents"
+}
+func TNDocumentStore() string {
+	return "md_document_store"
+}
+
+func TNAttachment() string {
+	return "md_attachment"
+}
+
+func TNRelationship() string {
+	return "md_relationship"
 }
 
 func TNMembers() string {
@@ -40,6 +65,23 @@ func TNFans() string {
 	return "md_fans"
 }
 
+func TNComments() string {
+	return "md_comments"
+}
+
+func TNScore() string {
+	return "md_score"
+}
+
+/*
+* Tool Funcs
+* */
+//设置增减
+//@param            table           需要处理的数据表
+//@param            field           字段
+//@param            condition       条件
+//@param            incre           是否是增长值，true则增加，false则减少
+//@param            step            增或减的步长
 func IncOrDec(table string, field string, condition string, incre bool, step ...int) (err error) {
 	mark := "-"
 	if incre {
