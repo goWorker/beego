e406da42685bfe44933044a50b1b9c3285918570
Yanzhao (Allan) Yang <allanyang@juniper.net>
2020-07-07 10:29:50 +0800

    add apiframwork

---
+++ apiFramework/router/router.go
@@ -0,0 +1,26 @@
+package router
+
+import (
+	"apiFramework/handler/sd"
+	"apiFramework/router/middleware"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Load(g *gin.Engine, mw ...gin.HandlerFunc) *gin.Engine {
+	g.Use(gin.Recovery())
+	g.Use(middleware.NoCache)
+	g.Use(middleware.Secure)
+	g.Use(mw...)
+	g.NoRoute(func(c *gin.Context) {
+		c.String(http.StatusNotFound, "The incorrect API route")
+	})
+	svcd := g.Group("/sd")
+	{
+		svcd.GET("/health", sd.HealthCheck)
+		svcd.GET("/disk", sd.DiskCheck)
+		svcd.GET("/cpu", sd.CPUCheck)
+		svcd.GET("/ram", sd.RAMCheck)
+	}
+	return g
+}
